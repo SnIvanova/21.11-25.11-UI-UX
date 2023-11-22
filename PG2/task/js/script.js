909aaba8c129d9466b4ca6aa98e301e7fc56cc75
SnIvanova <1snizhanaivanova@gmail.com>
2023-11-22 18:45:39 +0100

    calendar

--- PG2/task/js/script.js
+++ PG2/task/js/script.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const scrollButton = document.querySelector('.fa-caret-square-up');
-    scrollButton.addEventListener('click', () => scrollToTop());
+    const calendarIcon = document.getElementById('calendar-icon');
+    const calendarTooltip = document.getElementById('calendar-tooltip');
+    
+    const date = new Date();
+    const daysInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
     const scrollToTop = () => {
       window.scrollTo({
@@ -8,14 +12,36 @@ document.addEventListener("DOMContentLoaded", () => {
         behavior: 'smooth'
       });
     };
-    window.addEventListener('scroll', function () {
+
+    const generateCalendar = () => {
+        let calendarHTML = '<div class="calendar">';
+        const today = new Date().getDate(); 
+        for(let i = 1; i <= daysInMonth; i++) {
+            const dayClass = i === today ? 'calendar-day today' : 'calendar-day';
+            calendarHTML += `<div class="${dayClass}">${i}</div>`;
+        }
+        calendarHTML += '</div>';
+        calendarTooltip.innerHTML = calendarHTML;
+    };
+    
+
+    scrollButton.addEventListener('click', scrollToTop);
+
+    window.addEventListener('scroll', () => {
         if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
           scrollButton.classList.add('pulse');
         } else {
           scrollButton.classList.remove('pulse');
         }
-      });
+    });
 
-
-  });
-  
+    calendarIcon.addEventListener('mouseenter', () => {
+        generateCalendar();
+        calendarTooltip.style.visibility = 'visible'; 
+    });
+    
+    calendarIcon.addEventListener('mouseleave', () => {
+        calendarTooltip.style.visibility = 'hidden'; 
+    });
+    
+});
