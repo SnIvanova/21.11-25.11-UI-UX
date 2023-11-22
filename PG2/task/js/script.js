799da7ff9f7d42c69e56a4c67d52d3800c4dbd79
SnIvanova <1snizhanaivanova@gmail.com>
2023-11-22 15:44:48 +0100

    changed mind

--- PG2/task/js/script.js
+++ PG2/task/js/script.js
@@ -1,20 +1,54 @@
-
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
+    // Scroll to top button functionality
     const scrollButton = document.querySelector('.fa-caret-square-up');
-
-    scrollButton.addEventListener('click', function () {
+    scrollButton.addEventListener('click', () => scrollToTop());
+  
+    // Social icon tooltip functionality
+    const handleMouseEvents = (icon, eventType) => {
+      if (eventType === 'mouseover' || eventType === 'focus') {
+        showTooltip(icon);
+      } else {
+        hideTooltip(icon);
+      }
+    };
+  
+    const handleClick = (icon) => hideTooltip(icon);
+  
+    const socialIcons = document.querySelectorAll('.floating-icons a');
+    socialIcons.forEach((icon) => {
+      icon.addEventListener('mouseover', () => handleMouseEvents(icon, 'mouseover'));
+      icon.addEventListener('mouseout', () => handleMouseEvents(icon, 'mouseout'));
+  
+      // Add focus and blur events for accessibility
+      icon.addEventListener('focus', () => handleMouseEvents(icon, 'focus'));
+      icon.addEventListener('blur', () => handleMouseEvents(icon, 'blur'));
+  
+      // Ensure tooltips are removed on icon click for touch devices
+      icon.addEventListener('click', () => handleClick(icon));
+    });
+  
+    // Function to scroll to the top smoothly
+    const scrollToTop = () => {
       window.scrollTo({
         top: 0,
         behavior: 'smooth'
       });
-    });
-    window.addEventListener('scroll', function () {
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-            scrollButton.style.display = 'none';
-        } else {
-            scrollButton.style.display = 'block';
-        }
-    });
-
-
+    };
+  
+    // Function to show tooltip
+    const showTooltip = (icon) => {
+      const tooltip = document.createElement('span');
+      tooltip.className = 'tooltip';
+      tooltip.textContent = icon.title;
+      icon.appendChild(tooltip);
+    };
+  
+    // Function to hide tooltip
+    const hideTooltip = (icon) => {
+      const tooltip = icon.querySelector('.tooltip');
+      if (tooltip) {
+        icon.removeChild(tooltip);
+      }
+    };
   });
+  
